b6d696ec5dc08b08811e3dd67fdafbb8fdb1e498
piyushk989 <piyushk989@gmail.com>
2025-02-25 23:51:51 +0530

    Demo class new haiu

---
+++ ERP_Fir_Se/src/test/java/demo/Democlass.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Democlass {
+
+	void fun()
+	{
+		
+		System.out.println("I am automnation engineer -1");
+	}
+	
+	void run()
+	{
+		
+		System.out.println("I am automnation engineer -2");
+	}
+	
+}
